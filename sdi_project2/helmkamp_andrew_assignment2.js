2b7fd9d84314255e7e88f1a52fb68fc365b798cd
Andrew Helmkamp <helmy@threebeersaday.com>
2012-11-03 00:01:30 -0400

    Added procedure, number function, and partial array function

--- sdi_project2/helmkamp_andrew_assignment2.js
+++ sdi_project2/helmkamp_andrew_assignment2.js
@@ -1,1 +1,61 @@
-alert("JavaScript works!");
+/***********
+*Andrew J. Helmkamp
+*SDI 1211
+*Project 2
+***********/
+
+var planeTickets = ["Logan's ticket", "Andrew's ticket", "Cherish's ticket"];
+var someString;
+var woot = 3;
+var hasTickets = true;
+
+var ticketCheck = function (hasTickets) {
+	if (hasTickets) {
+		console.log("Finally you have your tickets!");
+	} else{
+		console.log("You missed your deadline. Now you won't be able to go.");
+	};
+};
+
+var boolFunction = function () {
+	if (true) {
+
+	} else{
+
+	};
+
+	return;
+};
+
+var canIGetAWootWoot = function (woot) {
+	while (woot > 1) {
+		console.log("WOOT!");
+		woot--;
+	}; 
+
+	return woot;
+};
+
+var stringFunction = function () {
+
+	return;
+};
+
+var arrayFunction = function (planeTickets) {
+	for (var i = planeTickets.length - 1; i >= 0; i--) {
+		console.log(planeTickets[i] + " has been issued.");
+	};
+
+	return;
+};
+
+//Function Calls
+arrayFunction(planeTickets);
+ticketCheck(hasTickets);
+
+if (hasTickets) { canIGetAWootWoot(woot); };
+
+
+
+
+
