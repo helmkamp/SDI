83ac4b163ae67f32375c845d1ce4246372408c5a
Andrew Helmkamp <helmy@threebeersaday.com>
2012-11-03 02:21:55 -0400

    Did more refactoring and completed project

--- sdi_project2/helmkamp_andrew_assignment2.js
+++ sdi_project2/helmkamp_andrew_assignment2.js
@@ -12,54 +12,61 @@ var hasTickets = true;
 var ticketCheck = function (hasTickets) {
 	if (hasTickets) {
 		console.log("Finally you have your tickets!");
-	} else{
+	} else {
 		console.log("You missed your deadline. Now you won't be able to go.");
 	};
 };
 
-var boolFunction = function () {
-	if (true) {
-
-	} else{
-
-	};
-
-	return;
+var everyoneHasSeats = function (numTickets, city) {
+	if (numTickets === 3) {
+		console.log("Logan will have a reserved seat on the flight to " + city + ".");
+		return true;
+	} else {
+		console.log("Logan will not have a reserved seat on the flight to " + city + ".");
+		return false;
+	}
 };
 
 var canIGetAWootWoot = function (woot) {
 	while (woot > 1) {
 		console.log("WOOT!");
 		woot--;
-	}; 
+	};
 
 	return woot;
 };
 
-var stringFunction = function () {
-
-	return;
+var comfortLevel = function (city, degreeOfComfort) {
+	var statement = "Your flight to " + city + " will be " + degreeOfComfort + ".";
+	return statement;
 };
 
 var assignTickets = function (planeTickets, numTickets) {
 	if (numTickets === 3 && hasTickets) {
 		var len = planeTickets.length;
-		for (var i = len - 1; i >= 0; i--) {
-			console.log(planeTickets[i] + " has been issued.");
+		for (var i = 0; i <= len; i++) {
+			console.log(planeTickets[i] + " has been issued for the flight to Grand Forks.");
 		};
-		return true;
+		return planeTickets;
 	};
 };
 
 //Function Calls
 
-
-
 if (hasTickets) { 
-	var assignTickets = assignTickets(planeTickets, 3);
-	if (assignTickets) {
+	var tickets = assignTickets(planeTickets, 3);
+	
+	if (tickets.length == 3) {
 		ticketCheck(hasTickets);
-		var woots = canIGetAWootWoot(woot); 	
+		var woots = canIGetAWootWoot(woot);
+		var seats = everyoneHasSeats(3, "Chicago");
+		if (seats) {
+			var comfort = comfortLevel("Chicago", "very comfortable");
+			console.log(comfort);
+		} else{
+			var comfort = comfortLevel("Chicago", "very uncomfortable");
+			console.log(comfort);
+		};
 	} else{
 		console.log("You seem to be missing a ticket or have too many. Please check to see if everyone has their ticket.");
 	};
