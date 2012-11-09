b8c7db22f07f550927b826a028a7f4bd7745442b
Andrew Helmkamp <helmy@threebeersaday.com>
2012-11-09 18:21:35 -0500

    added json file

---
+++ sdi-project3/helmkamp_andrew_assignment3.js
@@ -0,0 +1,6 @@
+/***********
+*Andrew J. Helmkamp
+*SDI 1211
+*Project 3
+***********/
+
---
+++ sdi-project3/helmkamp_andrew_json.js
@@ -0,0 +1,35 @@
+/***********
+*Andrew J. Helmkamp
+*SDI 1211
+*Project 3
+*Data File
+***********/
+
+var data = {
+	"vehicles": {
+		"0001": {
+			"makeModel": "Chevrolet Impala LT",
+			"year": "2011",
+			"milage": "42943",
+			"price": "14,400"
+		},
+		"0002": {
+			"makeModel": "Mitsubishi Endeavor SE",
+			"year": "2007",
+			"milage": "85520",
+			"price": "12,900"
+		},
+		"0003": {
+			"makeModel": "Dodge Avenger SXT",
+			"year": "2012",
+			"milage": "14088",
+			"price": "15,900"
+		},
+		"0004": {
+			"makeModel": "",
+			"year": "",
+			"milage": "",
+			"price": ""
+		}
+	}
+};
