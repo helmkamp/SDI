3b04ddb66121fc7859e12aa96542d0844b970bba
Andrew Helmkamp <helmy@threebeersaday.com>
2012-11-14 15:56:32 -0600

    added console logs for my library methods

--- sdi-project4/helmkamp_andrew_assignment4.js
+++ sdi-project4/helmkamp_andrew_assignment4.js
@@ -1,1 +1,11 @@
-alert("JavaScript works!");
+/***********
+*Andrew J. Helmkamp
+*SDI 1211
+*Project 4
+***********/
+
+var myLib = new myLibrary();
+
+console.log("Phone number test: " + myLib.phonePattern("123-456-7890"));
+console.log("Email address test: " + myLib.emailPattern("helmy@fullsail.edu"));
+console.log("URL test: " + myLib.urlPattern("http://www.fullsail.com"));
