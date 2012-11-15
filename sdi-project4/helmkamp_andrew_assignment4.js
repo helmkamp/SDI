3a89c475cdf80b2a37f7f252009a49bb3a6cb02e
Andrew Helmkamp <helmy@threebeersaday.com>
2012-11-15 15:07:58 -0600

    Added two more methods for arrays
    findSmallestNumber method will sort an array and then find the smallest
    number that is greater than the given number.
    
    sumArray will take an array and give you the sum of all the numbers in
    the array.

--- sdi-project4/helmkamp_andrew_assignment4.js
+++ sdi-project4/helmkamp_andrew_assignment4.js
@@ -4,8 +4,12 @@
 *Project 4
 ***********/
 
+//Array to send to the smallest number method
+var smallArr = [1,2,5,12,3,8,9,4]
+//Array to send to the adding method
+var sumArr = [2, "34 43", 3];
 //Array of objects to send to the sorting method
-var myArr = [{a:2}, {a:3}, {a:1}];
+var sortArr = [{a:2}, {a:3}, {a:1}];
 
 
 var myLib = new myLibrary();
@@ -18,4 +22,6 @@ console.log(myLib.replaceSeperator("my,shopping,list"));
 console.log("Your formated number is " + myLib.formatToCurrency(10.323423234));
 console.log("The time difference is " + myLib.timeDifference("2012, 4, 11", "2012, 4, 12") + " hours.");
 console.log("Number: ", myLib.stringToNumber("23"));
-console.log(myLib.sortArray(myArr));
+console.log(myLib.findSmallestNumber(smallArr, 2));
+console.log(myLib.sumArray(sumArr));
+console.log(myLib.sortArray(sortArr));
--- sdi-project4/helmkamp_andrew_library.js
+++ sdi-project4/helmkamp_andrew_library.js
@@ -82,9 +82,30 @@ var myLibrary = function() {
 	};
 
 //Array methods
-	//This method will find the smallest number in an array that is greater than a given number
+	//This method will find the smallest number in an array that is greater 
+	//than a given number
+	var findSmallestNumber = function(arr, num) {
+		var number = num;
+		
+		arr.sort(function(a,b){return a-b});
+
+		for (i=0; i<arr.length; i++) {
+			if ((arr[i] > num) && (arr[i] < arr[i]+1)) {
+				 return arr[i];
+			};
+		};
+	};
 
 	//This method will find the total value of just the numbers in an array
+	var sumArray = function(arr) {
+		var sum = 0;
+		for (i=0; i<arr.length; i++) {
+			if (isFinite(arr[i])) {
+				sum += arr[i];
+			};
+		};
+		return sum;
+	};
 
 	//This method will sort an array of objects
 	var sortArray = function(obs) {
@@ -95,15 +116,17 @@ var myLibrary = function() {
 
 
 	return {
-		"phonePattern":     phonePattern,
-		"emailPattern":     emailPattern,
-		"urlPattern":       urlPattern,
-		"titleCase":        titleCase,
-		"replaceSeperator": replaceSeperator,
-		"formatToCurrency": formatToCurrency,
-		"timeDifference":   timeDifference,
-		"stringToNumber":   stringToNumber,
-		"sortArray":        sortArray
+		"phonePattern":       phonePattern,
+		"emailPattern":       emailPattern,
+		"urlPattern":         urlPattern,
+		"titleCase":          titleCase,
+		"replaceSeperator":   replaceSeperator,
+		"formatToCurrency":   formatToCurrency,
+		"timeDifference":     timeDifference,
+		"stringToNumber":     stringToNumber,
+		"findSmallestNumber": findSmallestNumber,
+		"sumArray":           sumArray, 
+		"sortArray":          sortArray
 	};
 };
 
