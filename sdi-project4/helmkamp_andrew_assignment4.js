472c826a9fdf7ad2093c0071a8325413c610645d
Andrew Helmkamp <helmy@threebeersaday.com>
2012-11-15 16:23:09 -0600

    Refactored code
    Updated some of the console log messages to give more info. Added the
    new libraries.

--- sdi-project4/helmkamp_andrew_assignment4.js
+++ sdi-project4/helmkamp_andrew_assignment4.js
@@ -12,16 +12,19 @@ var sumArr = [2, "34 43", 3];
 var sortArr = [{a:2}, {a:3}, {a:1}];
 
 
-var myLib = new myLibrary();
+var stringLib = new stringLibrary();
+var mathLib = new mathLibrary();
+var arrayLib = new arrayLibrary();
 
-console.log("Phone number test: " + myLib.phonePattern("123-456-7890"));
-console.log("Email address test: " + myLib.emailPattern("helmy@fullsail.edu"));
-console.log("URL test: " + myLib.urlPattern("http://www.fullsail.com"));
-console.log(myLib.titleCase("This is a title"));
-console.log(myLib.replaceSeperator("my,shopping,list"));
-console.log("Your formated number is " + myLib.formatToCurrency(10.323423234));
-console.log("The time difference is " + myLib.timeDifference("2012, 4, 11", "2012, 4, 12") + " hours.");
-console.log("Number: ", myLib.stringToNumber("23"));
-console.log(myLib.findSmallestNumber(smallArr, 2));
-console.log(myLib.sumArray(sumArr));
-console.log(myLib.sortArray(sortArr));
+console.log("Phone number test: " + stringLib.phonePattern("123-456-7890"));
+console.log("Email address test: " + stringLib.emailPattern("helmy@fullsail.edu"));
+console.log("URL test: " + stringLib.urlPattern("http://www.fullsail.com"));
+console.log(stringLib.titleCase("This is a title"));
+console.log(stringLib.replaceSeperator("my,shopping,list"));
+console.log("Your formated number is " + mathLib.formatToCurrency(10.323423234));
+console.log("Percentage Test: " + mathLib.percentTest(4,6));
+console.log("The time difference is " + mathLib.timeDifference("2012, 4, 11", "2012, 4, 12") + " hours.");
+console.log("Number: ", mathLib.stringToNumber("23"));
+console.log("The smallest number that's greater than your number is: " + arrayLib.findSmallestNumber(smallArr, 2));
+console.log("The sum of your array is: " + arrayLib.sumArray(sumArr));
+console.log("Sorted Array: ", arrayLib.sortArray(sortArr));
